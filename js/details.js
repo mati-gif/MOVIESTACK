650373fab062bcd4c2e1ee5e95cde27b4341fb1c
mati-gif <matiassoria7700@gmail.com>
2024-07-15 15:26:06 -0300

    cambiandole los estilos para que se vean mejor

--- js/details.js
+++ js/details.js
@@ -19,15 +19,30 @@ function createDetailsCard(peliculaDetalle){//este paraemtro es un objeto que co
     let contenedorDetalle = document.getElementById("detail_container");
     
 
-let card = `<div class=" m-auto border-black border-2 mt-5 mb-10  flex  items-start justify-beetwen w-11/12 h-full text-center bg-violet-200 ">
-    <img src =${peliculaDetalle.image} class="h-64 w-80 object-cover  sm: w-60 h-28"  alt = ${peliculaDetalle.title}/>
-    <div class="flex flex-col h-auto w-4/5  item-center justify-start border-black border-2 bg-green-400">
-    <h1 class=" font-bold text-3xl m-5">${peliculaDetalle.title}</h1>
-    <h2 class=" font-bold text-xl m-5 ">${peliculaDetalle.tagline}</h2>
-    <h3 class=" font-bold text-xl m-5">${peliculaDetalle.genres}</h3>
-    <p class=" font-bold text-xl m-5">${peliculaDetalle.overview}</p>
-    </div>
+let card = `<div class=" m-auto  mt-5 mb-10  flex flex-col  justify-evenly w-96  text-center  ">
+    <img src =${peliculaDetalle.image} class="h-64 w-96 object-cover  sm: w-60 h-28"  alt = ${peliculaDetalle.title}/>
+    
 
+	<table class=" bg-white rounded-lg shadow-md ">
+	<tbody>
+		<tr class="border-2 border-black bg-green-200">
+			<td class="px-4  border-2 border-black py-2 text-left text-gray-700 font-medium"> original languaje</td>
+			<td class="px-4 border-2 border-black  py-2 text-gray-600">${peliculaDetalle.original_language}</td>
+		</tr>
+		<tr class="border-2 border-black">
+			<td class="px-4 border-2 border-black py-2 text-left text-gray-700 font-medium">realese data</td>
+			<td class="px-4 border-2 border-black py-2 text-gray-600">${peliculaDetalle.release_date}</td>
+		</tr>
+		<tr class="border-2 border-black">
+			<td class="px-4 border-2 border-black py-2 text-left text-gray-700 font-medium">runtime</td>
+			<td class="px-4 border-2 border-black py-2 text-gray-600">${peliculaDetalle.runtime}</td>
+		</tr>
+		<tr class="border-2 border-black">
+			<td class="px-4 border-2 border-black py-2 text-left text-gray-700 font-medium">status</td>
+			<td class="px-4 border-2 border-black py-2 text-gray-600">${peliculaDetalle.status}</td>
+		</tr>
+	</tbody>
+    </table>
 
     </div>
 `
@@ -39,42 +54,27 @@ contenedorDetalle.innerHTML += card;
 function crearTabla(contenedorDetalle,peliculaDetalle){
 
 
-let tablas  =  `<div class=" flex  justify-evenly "> 
-<table class=" bg-white border-2 border-gray-200 rounded-lg shadow-md ">
-	<tbody>
-		<tr class="border-2 border-black ">
-			<td class="px-4 py-2 text-left text-gray-700 font-medium"> original languaje</td>
-			<td class="px-4 py-2 text-gray-600">${peliculaDetalle.original_language}</td>
-		</tr>
-		<tr class="border-2 border-black">
-			<td class="px-4 py-2 text-left text-gray-700 font-medium">realese data</td>
-			<td class="px-4 py-2 text-gray-600">${peliculaDetalle.release_date}</td>
-		</tr>
-		<tr class="border-2 border-black">
-			<td class="px-4 py-2 text-left text-gray-700 font-medium">runtime</td>
-			<td class="px-4 py-2 text-gray-600">${peliculaDetalle.runtime}</td>
-		</tr>
-		<tr class="border-2 border-black">
-			<td class="px-4 py-2 text-left text-gray-700 font-medium">status</td>
-			<td class="px-4 py-2 text-gray-600">${peliculaDetalle.status}</td>
-		</tr>
-	</tbody>
-    </table>
-    
-    <table class=" bg-white  border-2 border-gray-200 rounded-lg shadow-md" >
+let tablas  =  `<div class=" m-auto border-black border-2 mt-5 mb-10  flex flex-col  w-2/4   "> 
+
+    <h1 class=" font-bold text-3xl m-5">${peliculaDetalle.title}</h1>
+    <h2 class=" font-bold text-xl m-5 ">${peliculaDetalle.tagline}</h2>
+    <h3 class=" font-bold text-xl m-5">${peliculaDetalle.genres}</h3>
+    <p class=" font-bold text-xl m-5">${peliculaDetalle.overview}</p> 
+
+   <table class=" w-96 h-40 bg-white m-auto border-2 border-gray-200 rounded-lg shadow-md" >
 	<tbody>
 		
-		<tr class="border-2 border-black">
-			<td class="px-4 py-2 text-left text-gray-700 font-medium">vote overage</td>
-			<td class="px-4 py-2 text-gray-600">${peliculaDetalle.vote_average}</td>
+		<tr class="border-2 border-black bg-green-200">
+			<td class="px-4 border-2 border-black py-2 text-left text-gray-700 font-medium">vote overage</td>
+			<td class="px-4 border-2 border-black py-2 text-gray-600">${peliculaDetalle.vote_average}</td>
 		</tr>
 		<tr class="border-2 border-black">
-			<td class="px-4 py-2 text-left text-gray-700 font-medium">budget</td>
-			<td class="px-4 py-2 text-gray-600">${peliculaDetalle.budget}</td>
+			<td class="px-4 border-2 border-black py-2 text-left text-gray-700 font-medium">budget</td>
+			<td class="px-4 border-2 border-black py-2 text-gray-600">${peliculaDetalle.budget}</td>
 		</tr>
 		<tr class="border-2 border-black" >
-			<td class="px-4 py-2 text-left text-gray-700 font-medium">revenue</td>
-			<td class="px-4 py-2 text-gray-600">${peliculaDetalle.revenue}</td>
+			<td class="px-4 border-2 border-black py-2 text-left text-gray-700 font-medium">revenue</td>
+			<td class="px-4 border-2 border-black py-2 text-gray-600">${peliculaDetalle.revenue}</td>
 		</tr>
 	</tbody>
 </table> 
@@ -86,3 +86,29 @@ let tablas  =  `<div class=" flex  justify-evenly ">
     contenedorDetalle.innerHTML += tablas;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* <div class="flex flex-col h-auto w-4/5  item-center justify-start border-black border-2 bg-green-400">    </div> */
+
+
+
+
+
+
+
